f61c7f00f6171e52d26f9ad920b8cd33636202b3
EvilArgest <argest7997@gmail.com>
2021-09-16 16:46:19 +0300

    Third task

--- web/modules/custom/evilargest/src/Controller/CatsController.php
+++ web/modules/custom/evilargest/src/Controller/CatsController.php
@@ -3,10 +3,10 @@
 namespace Drupal\evilargest\Controller;
 
 class CatsController {
-  public function content() {
-    $element = array(
-      '#markup' => 'Hello! You can add here a photo of your cat.',
-    );
-    return $element;
+  public function content()
+  {
+    return [
+      '#theme' => 'cats-theme',
+    ];
   }
 }
